0b465f96cd4c8e112a32fc0ee3677801e9e83e57
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-08-07 16:24:32 -0400

    Created CRUD Queries in ./scratch/queries.js

--- scratch/queries.js
+++ scratch/queries.js
@@ -2,7 +2,7 @@
 
 const knex = require('../knex');
 
-let searchTerm = 'gaga';
+let searchTerm = 'Steve Jobs';
 knex
   .select('notes.id', 'title', 'content')
   .from('notes')
@@ -18,3 +18,48 @@ knex
   .catch(err => {
     console.error(err);
   });
+
+let searchId = 1;
+
+// knex
+//   .first('notes.id', 'title', 'content')
+//   .from('notes')
+//   .where('id', searchId)
+//   .then((results) => {
+//     console.log(JSON.stringify(results));
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+// knex('notes')
+//   .where('id', 1)
+//   .update({title: 'How to Win Friends and Influence People2018'})
+//   .returning(['id', 'title', 'content'])
+//   .then(results => {
+//     console.log(results);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+// knex('notes')
+//   .insert({title: 'THIS IS A TITLE', content: 'THIS IS CONTENT'})
+//   .returning(['id', 'title', 'content'])
+//   .then(result => {
+//     console.log(result);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+// knex('notes')
+//   .where({id: 6})
+//   .del()
+//   .then(result => {
+//     console.log('worked');
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+  
